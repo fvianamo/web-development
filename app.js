3d31e51abe03d29ce9f18eadf124db9379f8e214
fvianamo <filipevianam@gmail.com>
2019-05-08 15:35:53 -0300

    Crud finalizado

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 var app = new function(){
 		//Initicializa as variaveis
-		this.tasks = new Array;
+		this.tasks = [];
 		
 		//função que inicializa as primeiras tasks, ao abrir a página
 		this.initialize = function(){
 			var task = {};
 			task = {
-				id: 1,
-				desc: "A Fazer",
+				desc: "Tarefa 1",
 				resp: "Ze Junior",
-				prazo: "01-01-2019",
+				prazo: "2019-01-01",
 				done: true
 			} ;
 			this.tasks.push(task);
 			task = {
-				id: 2,
-				desc: "Fazendo",
+				desc: "Tarefa 2",
 				resp: "Ze Junior",
-				prazo: "01-01-2019",
+				prazo: "2019-01-01",
 				done: false
 			} ;
 			this.tasks.push(task);
 			task = {
-				id: 3,
-				desc: "Feito",
+				desc: "Tarefa 3",
 				resp: "Ze Junior",
-				prazo: "01-01-2019",
+				prazo: "2019-01-01",
 				done: false
 			} ;
 			this.tasks.push(task);
@@ -34,58 +31,78 @@ var app = new function(){
 
 		//Função que gera o HTML das listas
 		this.list_all = function(){
-
-			var table = document.getElementById('todo-list');
-
-			//Inserting tasks
-			for (var i =0; i < this.tasks.length; i++){
-				row = table.insertRow(i);
-				
-				descCell = row.insertCell(0);
-				respCell = row.insertCell(1);
-				prazoCell = row.insertCell(2);
-
-				if (!this.tasks[i].done) {
-					descCell.innerHTML = '<strike>'+this.tasks[i].desc+'</strike>';
-					respCell.innerHTML = '<strike>'+this.tasks[i].resp+'</strike>';
-					prazoCell.innerHTML = '<strike>'+this.tasks[i].prazo+'</strike>';
+			//Cleaning table element
+			document.getElementById('todo-list').innerHTML = "";
+			//Escreve a lista!
+			for (var i = 0; i < this.tasks.length; i++){
+				if (this.tasks[i].done==false) {
+					document.getElementById('todo-list').innerHTML += '<tr><td>' + this.tasks[i].desc 
+					+ '</td><td>' + this.tasks[i].resp 
+					+ '</td><td>' + this.tasks[i].prazo 
+					+ '</td><td><button onclick="app.done()" value="' + i + '">feito</button></td><td><button onclick="app.edit()" value="' + i + '">edit</button></td><td><button onclick="app.delete()" value="' + i + '">deletar</button></td></tr>';
 				}
 				else{
-					descCell.innerHTML = this.tasks[i].desc;
-					respCell.innerHTML = this.tasks[i].resp;
-					prazoCell.innerHTML = this.tasks[i].prazo;
+					document.getElementById('todo-list').innerHTML += '<tr><td><strike>' + this.tasks[i].desc 
+					+ '</strike></td><td><strike>' + this.tasks[i].resp 
+					+ '</strike></td><td><strike>' + this.tasks[i].prazo 
+					+ '</strike></td><td><button onclick="app.done()" value="' + i + '">feito</button></td><td><button onclick="app.edit()" value="' + i + '">edit</button></td><td><button onclick="app.delete()" value="' + i + '">deletar</button></td></tr>';
 				}
-
 			}
 		}
 
 		//função que adiciona uma task na lista, sempre inicia como A Fazer
 		this.add = function(){
+			var _desc = document.getElementById('desc').value;
+			var _resp = document.getElementById('resp').value;
+			var _prazo = document.getElementById('prazo').value;
+
 			var _task = {
-				id: 4,
-				desc: "testando",
-				resp: "Vou dizer?",
-				prazo: "31-12-2018",
+				desc: _desc,
+				resp: _resp,
+				prazo: _prazo,
 				done: false
 			};
 			this.tasks.push(_task);
+
+			document.getElementById('desc').value = '';
+			document.getElementById('resp').value = '';
+			document.getElementById('prazo').value = '';
+
 			this.list_all();
 		}
 
 		//função que edita task da lista
 		this.edit = function(){
-			//code goes here
+			//limpa formulário antes de preencher para modificar
+			document.getElementById('desc').value = '';
+			document.getElementById('resp').value = '';
+			document.getElementById('prazo').value = '';
+			
+			var _task = this.tasks[parseInt(event.srcElement.value)];
+			this.tasks.splice(parseInt(event.srcElement.value),1);
+
+			document.getElementById('desc').value = _task.desc;
+			document.getElementById('resp').value = _task.resp;
+			document.getElementById('prazo').value = _task.prazo;
+
+			this.list_all();
 		}
 
 		//função que move a task para o próximo estado
-		this.foward = function(){
-			//code goes here
+		this.done = function(){
+			//console.log(event.srcElement.value);
+			var _task = this.tasks[parseInt(event.srcElement.value)];
+			_task.done = true;
+			this.tasks.splice(parseInt(event.srcElement.value),1);
+			this.tasks.push(_task);
+			this.list_all();
 		}
 
 		//função que remove a task da lista
-		this.remove = function(){
-			//code goes here
+		this.delete = function(){
+			//console.log(event.srcElement.value);
+			this.tasks.splice(parseInt(event.srcElement.value),1);
+			this.list_all();
 		}
 	}
-app.add();
 app.initialize();
